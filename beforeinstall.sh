5e9575a991fb41ffd0118814d32b0936bc0b7bc6
kunalbhole <113830783+kunalbhole99@users.noreply.github.com>
2023-05-30 14:49:39 +0530

    Update beforeinstall.sh

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -xe
 cd /home/ec2-user
 sudo rm -rf *
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y apache-maven
+yum install java-1.8.0-openjdk-devel.x86_64 -y
+yum install zip -y
 PID=`ps -eaf | grep webapp | grep -v grep | awk '{print $2}'`
 if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"
