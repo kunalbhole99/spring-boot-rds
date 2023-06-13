e9ba3177e05fcf1626e855513be0aeb1d8531b6e
kunalbhole <113830783+kunalbhole99@users.noreply.github.com>
2023-06-13 14:27:53 +0530

    Update MainController.java

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -17,7 +17,8 @@ public class MainController {
 
 	@PostMapping(path="/register/post") // Map ONLY POST Requests
 	public @ResponseBody String addNewUser (@RequestParam String name
-			, @RequestParam String email , @RequestParam String department) {
+			, @RequestParam String email 
+						, @RequestParam String department) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
